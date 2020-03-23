fba8bc18228ff94ab5e59b79e545f28b348d8a55
armin-tahirovic <43197986+armin-tahirovic@users.noreply.github.com>
2020-03-23 11:54:19 +0100

    Add files via upload

---
+++ Insert i DW Fact.sql
@@ -0,0 +1,23 @@
+Insert into [northwindDW].[dbo].[SalesFact] 
+(      [P_ID]
+      ,[C_ID]
+      ,[E_ID]
+      ,[D_ID]
+      ,[Quantity]
+      ,[SalesAmount])
+      select p.[P_ID]
+      ,c.[C_ID]
+      ,e.[E_ID]
+      ,od.[D_ID]
+      ,f.[SalesAmount]
+      ,f.[Quantity]
+
+      from [northwindStage].[dbo].[Fact_sales] f 
+      left join [northwindDW].[dbo].[Product_Dimension] as p
+      on p.ProductId = f.ProductID
+      left join  [dbo].[Customer_Dimension] as c
+      on c.CustomerId=f.CustomerId
+      left join [dbo].[Employee_Dimension] as e
+      on e.EmployeeId=f.EmployeeId
+      left join [dbo].[Date_Dimension] as od
+      on od.DateID = f.DateID
