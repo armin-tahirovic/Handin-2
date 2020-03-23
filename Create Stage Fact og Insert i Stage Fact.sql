6b490a7f154ef5e9a3d64be59d7e6a4a36c2057c
armin-tahirovic <43197986+armin-tahirovic@users.noreply.github.com>
2020-03-23 10:52:39 +0100

    Add files via upload

---
+++ Create Stage Fact og Insert i Stage Fact.sql
@@ -0,0 +1,36 @@
+USE northwindStage
+CREATE TABLE Fact_sales (
+ C_ID INT NULL,
+ E_ID INT NULL,
+ P_ID INT NULL,
+ D_ID INT NULL,
+ CustomerID NCHAR(5) NULL,
+ DateID DATE NULL,
+ EmployeeID INT NULL,
+ ProductID INT NULL,
+ Quantity int,
+ SalesAmount decimal (18,2)
+);
+
+
+USE northwindDB
+GO
+INSERT INTO northwindStage.dbo.Fact_sales (
+SalesAmount,
+Quantity,
+ProductID,
+CustomerID,
+EmployeeID,
+DateID)
+SELECT
+Quantity,
+[Order Details].UnitPrice * Quantity,
+Products.ProductID,
+Customers.CustomerID,
+Employees.EmployeeID,
+Orders.OrderDate
+FROM [Order Details]
+JOIN Orders ON Orders.OrderID = [Order Details].OrderID
+JOIN Customers ON Orders.CustomerID = Customers.CustomerID
+JOIN Employees ON Employees.EmployeeID = Orders.EmployeeID
+JOIN Products ON Products.ProductID = [Order Details].ProductID
---
+++ Create Stage Fact.sql
@@ -0,0 +1,7 @@
+CREATE TABLE SalesFact (
+[P_ID] int not null,
+[C_ID] nchar(5) not null,
+[E_ID] int not null,
+[D_ID] int not null,
+[Quantity] int not null,
+[SalesAmount] int not null)
